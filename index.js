b3c2d88a3802a44bf681658d8b3737694f47c617
christophermichael <cmichaelcolonna@gmail.com>
2023-02-15 13:03:28 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,47 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  "Hello!".toLowerCase("hello"); // 'HELLO!'
+
+  function logShout(_string) {
+    console.log('HELLO')
+  }
+
+  console.log.restore.toUpperCase('HELLO');
+
+  function logWhisper(_string) {
+    console.log('hello');
+}
+
+function sayHiToHeadphonedRoommate(string){
+  
+  if (string === 'hello'){
+    return 'I can\'t hear you!';
+  }
+  else if(string === `HELLO`){
+    return ('YES INDEED!');
+  }
+  else if(string === "Let\'s have dinner together!"){
+    return ('I would love to!');
+  }
+}
+
+  
+ console.log()
+  
+
+  
+
+  //console.log('hello')
+  //return 'I can\'t hear you!';
+
+
+
+
+  
+
